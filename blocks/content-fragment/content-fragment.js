ef7c7b92eb6ca21b58dec4edee147c1b8fd779c9
Michael Smith <michasmi@adobe.com>
2025-09-04 12:24:27 -0500

    Update content-fragment.js
    array appends and join

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -139,21 +139,21 @@ export default async function decorate(block) {
 				*/
 				const elements = [];
 			
-			  elements.append(`<div class='banner-content block ${displayStyle}' data-aue-resource=${itemId} data-aue-label="Offer Content fragment" data-aue-type="reference" data-aue-filter="contentfragment" style="${bannerContentStyle}">
-          <div class='banner-detail' style="${bannerDetailStyle}" data-aue-prop="bannerimage" data-aue-label="Main Image" data-aue-type="media" >
-                <p data-aue-prop="title" data-aue-label="Title" data-aue-type="text" class='cftitle'>${cfReq?.title}</p>
-                <p data-aue-prop="cfsubtitle" data-aue-label="SubTitle" data-aue-type="text" class='cfsubtitle'>${cfReq?.subtitle}</p>
+			  elements.append("<div class='banner-content block ${displayStyle}' data-aue-resource=${itemId} data-aue-label='Offer Content fragment' data-aue-type='reference' data-aue-filter='contentfragment' style='${bannerContentStyle}'>
+          <div class='banner-detail' style='${bannerDetailStyle}' data-aue-prop='bannerimage' data-aue-label='Main Image' data-aue-type='media' >
+                <p data-aue-prop='title' data-aue-label='Title' data-aue-type='text' class='cftitle'>${cfReq?.title}</p>
+                <p data-aue-prop='cfsubtitle' data-aue-label='SubTitle' data-aue-type='text' class='cfsubtitle'>${cfReq?.subtitle}</p>
                 
-                <p data-aue-prop="cfdescription" data-aue-label="Description" data-aue-type="richtext" class='cfdescription'>${cfReq?.description?.plaintext}</p>
-								<div class="button-group">`);
+                <p data-aue-prop='cfdescription' data-aue-label='Description' data-aue-type='richtext' class='cfdescription'>${cfReq?.description?.plaintext}</p>
+								<div class='button-group'>");
 				
-				elements.append(`<p class="button-container primary">
-				 						<strong><a href="${cfReq?.urlmain ? cfReq.urlmain : '#'}" contenteditable="true" title="${cfReq?.ctalabelmain}" class="button">${cfReq?.ctalabelmain}</a></strong>
-					 				</p>`);
+				elements.append("<p class='button-container primary'>
+				 						<strong><a href='${cfReq?.urlmain ? cfReq.urlmain : \"#\"}' contenteditable='true' title='${cfReq?.ctalabelmain}' class='button'>${cfReq?.ctalabelmain}</a></strong>
+					 				</p>");
 				
-				elements.append(`<p class="button-container secondary">
-				 						<strong><a href="${cfReq?.urlsecondary ? cfReq.urlsecondary : '#'}" contenteditable="true" title="${cfReq?.ctalabelsecondary}" class="button">${cfReq?.ctalabelsecondary}</a></strong>
-					 				</p>`);
+				elements.append("<p class='button-container secondary'>
+				 						<strong><a href='${cfReq?.urlsecondary ? cfReq.urlsecondary : \"#\"}' contenteditable='true' title='${cfReq?.ctalabelsecondary}' class='button'>${cfReq?.ctalabelsecondary}</a></strong>
+					 				</p>");
 				<!--				
 				        <span>
 	                <a href="${cfReq?.urlmain ? cfReq.urlmain : '#'}" data-aue-prop="ctaUrl" data-aue-label="Button Link/URL" data-aue-type="reference" class="button primary" target="_blank" rel="noopener" data-aue-filter="page">
@@ -169,11 +169,11 @@ export default async function decorate(block) {
 								</span>
 				-->
 			
-        elements.append(`</div></div>
+        elements.append("</div></div>
             <div class='banner-logo'>
-							<img src="${imgUrl}" data-aue-prop="image" data-aue-label="Image" data-aue-type="media">
+							<img src='${imgUrl}' data-aue-prop='image' data-aue-label='Image' data-aue-type='media'>
             </div>
-        </div>`);
+        </div>");
 
 			blocks.innerHTML = elements.join("");
 
