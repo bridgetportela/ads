d4a4c9c8e687710d26695394f5718d39ff53b5d4
Michael Smith <michasmi@adobe.com>
2025-09-03 21:11:03 -0500

    Update content-fragment.js
    update response query

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -92,7 +92,7 @@ export default async function decorate(block) {
           return;
         }
 
-        const cfReq = offer?.data?.ctaByPath?.item;
+        const cfReq = offer?.data?.adsProductByPath?.item;
 
         if (!cfReq) {
           console.error('Error parsing response from GraphQL request - no valid data found', {
