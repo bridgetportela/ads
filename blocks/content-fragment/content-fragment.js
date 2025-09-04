1e0ed83f1a31e86b416a26cd2002db5d788d0774
Michael Smith <michasmi@adobe.com>
2025-09-04 11:26:42 -0500

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -137,6 +137,39 @@ export default async function decorate(block) {
           bannerDetailStyle = 'background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%) ,url('+imgUrl+');';
         }
 				*/
+			  block.innerHTML = `<div class='banner-content block ${displayStyle}' data-aue-resource=${itemId} data-aue-label="Offer Content fragment" data-aue-type="reference" data-aue-filter="contentfragment" style="${bannerContentStyle}">
+          <div class='banner-detail' style="${bannerDetailStyle}" data-aue-prop="bannerimage" data-aue-label="Main Image" data-aue-type="media" >
+                <p data-aue-prop="title" data-aue-label="Title" data-aue-type="text" class='cftitle'>${cfReq?.title}</p>
+                <p data-aue-prop="cfsubtitle" data-aue-label="SubTitle" data-aue-type="text" class='cfsubtitle'>${cfReq?.subtitle}</p>
+                
+                <p data-aue-prop="cfdescription" data-aue-label="Description" data-aue-type="richtext" class='cfdescription'>${cfReq?.description?.plaintext}</p>
+								<div class="button-group">
+									<p class="button-container">
+				 						<strong><a href="${cfReq?.urlsecondary ? cfReq.urlsecondary : '#'}" contenteditable="true" title="${cfReq?.ctalabelsecondary}" class="button secondary">${cfReq?.ctalabelsecondary}</a></strong>
+					 				</p>
+									<p class="button-container primary">
+				 						<strong><a href="${cfReq?.urlmain ? cfReq.urlmain : '#'}" contenteditable="true" title="${cfReq?.ctalabelmain}" class="button primary">${cfReq?.ctalabelmain}</a></strong>
+					 				</p>
+								</div>
+				<!--				
+				        <span>
+	                <a href="${cfReq?.urlmain ? cfReq.urlmain : '#'}" data-aue-prop="ctaUrl" data-aue-label="Button Link/URL" data-aue-type="reference" class="button primary" target="_blank" rel="noopener" data-aue-filter="page">
+	                  <p data-aue-prop="ctalabelmain" data-aue-label="Button Label" data-aue-type="text">
+	                    ${cfReq?.ctalabelmain}
+	                  </p>
+	                </a>
+									<a href="${cfReq?.urlsecondary ? cfReq.urlsecondary : '#'}" data-aue-prop="ctaUrl" data-aue-label="Button Link/URL" data-aue-type="reference" class="button secondary" target="_blank" rel="noopener" data-aue-filter="page">
+	                  <p data-aue-prop="ctalabelsecondary" data-aue-label="Button Label" data-aue-type="text">
+	                    ${cfReq?.ctalabelsecondary}
+	                  </p>
+	                </a>
+								</span>
+				-->
+            </div>
+            <div class='banner-logo'>
+							<img src="${imgUrl}" data-aue-prop="image" data-aue-label="Image" data-aue-type="media">
+            </div>
+        </div>`;
 
 			/*
 				let mainButton = '';
@@ -153,7 +186,7 @@ export default async function decorate(block) {
 				 								+	'<strong><a href="${cfReq?.urlsecondary ? cfReq.urlsecondary : \'#\'}" contenteditable="true" title="${cfReq?.ctalabelsecondary}" class="button">${cfReq?.ctalabelsecondary}</a></strong>'
 					 						+	'</p>';
 				}
-*/
+
         block.innerHTML = '<div class="banner-content block ${displayStyle}" data-aue-resource=${itemId} data-aue-label="Offer Content fragment" data-aue-type="reference" data-aue-filter="contentfragment" style="${bannerContentStyle}">
          <div class="banner-detail" style="${bannerDetailStyle}" data-aue-prop="bannerimage" data-aue-label="Main Image" data-aue-type="media" >
              <p data-aue-prop="title" data-aue-label="Title" data-aue-type="text" class='cftitle'>${cfReq?.title}</p>
@@ -161,11 +194,9 @@ export default async function decorate(block) {
                 
              <p data-aue-prop="cfdescription" data-aue-label="Description" data-aue-type="richtext" class='cfdescription'>${cfReq?.description?.plaintext}</p>
 						 <div class="button-group"> 
-		
-					/*
+
 		 					+ mainButton
-							+ secButton
-			    */
+							+ secButton			    
 					
 			 			 <p class="button-container primary">
 				 				<strong><a href="${cfReq?.urlmain ? cfReq.urlmain : '#'}" contenteditable="true" title="${cfReq?.ctalabelmain}" class="button">${cfReq?.ctalabelmain}</a></strong>
@@ -179,6 +210,7 @@ export default async function decorate(block) {
 							<img src="${imgUrl}" data-aue-prop="image" data-aue-label="Image" data-aue-type="media">
             </div>
         </div>';
+				*/
         
     
       } catch (error) {
